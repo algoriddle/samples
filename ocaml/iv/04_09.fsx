6bd795673e9cd51cd79273e13d32e6d7359bab42
algoriddle <szilvasy@gmail.com>
2014-07-13 23:37:30 +0100

    iv 4.9 cleanup

--- ocaml/iv/04_09.fsx
+++ ocaml/iv/04_09.fsx
@@ -12,29 +12,31 @@ let test = Node (2,
                        Node (0, Node (-1, Node (1, Empty, Empty), Node (2, Empty, Empty)),
                              Node (-2, Node (-1, Empty, Empty), Node (2, Empty, Empty)))));;
 
-(* add to 'acc' those sublists of 'list' that begin with head and sum to 'sum' *)
-let sublists acc list sum =
+(* add to 'lists' those sublists of 'list' that begin with head and sum to 'sum' *)
+let sublists list sum lists =
   let (res, _, _) = List.fold (fun (ls, l, s) x ->
       let ns = s + x in
       let nl = x :: l in
       if ns = sum
       then (nl :: ls, nl, ns)
-      else (ls, nl, ns)) (acc, [], 0) list
+      else (ls, nl, ns)) (lists, [], 0) list in
   res;;
 
-sublists [] [2; 2; -2; 2; 0; 1] 4;;
+sublists [2; 2; -2; 2; 0; 1] 4 [];;
 
 (* find paths in the 'tree' that sum to 'sum' *)
 let paths tree sum =
-  let rec aux paths path = function
+  let rec aux tree path paths =
+    match tree with
     | Empty -> paths
     | Node (x, left, right) ->
       let new_path = x :: path in
-      let new_paths = sublists paths new_path sum in
-      let left_paths = aux new_paths new_path left in
-      aux left_paths new_path right
+      paths
+      |> sublists new_path sum
+      |> aux left new_path
+      |> aux right new_path
   in
-  aux [] [] tree
+  aux tree [] []
 ;;
 
 paths test 0;;
--- ocaml/iv/04_09.ml
+++ ocaml/iv/04_09.ml
@@ -11,9 +11,9 @@ Random.init 12345;;
 
 let test = generate 5;;
 
-(* add to 'acc' those sublists of 'list' that begin with head and sum to 'sum' *)
-let sublists acc list sum =
-  let (res, _, _) = List.fold list ~init:(acc, [], 0) ~f:(fun (ls, l, s) x ->
+(* add to 'lists' those sublists of 'list' that begin with head and sum to 'sum' *)
+let sublists list sum lists =
+  let (res, _, _) = List.fold list ~init:(lists, [], 0) ~f:(fun (ls, l, s) x ->
       let ns = s + x in
       let nl = x :: l in
       if ns = sum
@@ -21,19 +21,21 @@ let sublists acc list sum =
       else (ls, nl, ns)) in
   res;;
 
-sublists [] [2; 2; -2; 2; 0; 1] 4;;
+sublists [2; 2; -2; 2; 0; 1] 4 [];;
 
 (* find paths in the 'tree' that sum to 'sum' *)
 let paths tree sum =
-  let rec aux paths path = function
+  let rec aux tree path paths =
+    match tree with
     | Empty -> paths
     | Node (x, left, right) ->
       let new_path = x :: path in
-      let new_paths = sublists paths new_path sum in
-      let left_paths = aux new_paths new_path left in
-      aux left_paths new_path right
+      paths
+      |> sublists new_path sum
+      |> aux left new_path
+      |> aux right new_path
   in
-  aux [] [] tree
+  aux tree [] []
 ;;
 
 paths test 0;;
