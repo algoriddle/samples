213f1a987bc879405de52a6588a7b7f66f0c5a46
algoriddle <szilvasy@gmail.com>
2014-07-13 23:20:56 +0100

    iv 4.9 formatting

--- ocaml/iv/04_09.fsx
+++ ocaml/iv/04_09.fsx
@@ -12,28 +12,29 @@ let test = Node (2,
                        Node (0, Node (-1, Node (1, Empty, Empty), Node (2, Empty, Empty)),
                              Node (-2, Node (-1, Empty, Empty), Node (2, Empty, Empty)))));;
 
-let accSublistsThatSumTo acc list sum =
-  let (res, _, _) =
-    List.fold (fun (ls, l, s) x ->
-               let ns = s + x
-               let nl = x :: l
-               if ns = sum
-               then (nl :: ls, nl, ns)
-               else (ls, nl, ns)) (acc, [], 0) list
+(* add to 'acc' those sublists of 'list' that begin with head and sum to 'sum' *)
+let sublists acc list sum =
+  let (res, _, _) = List.fold (fun (ls, l, s) x ->
+      let ns = s + x in
+      let nl = x :: l in
+      if ns = sum
+      then (nl :: ls, nl, ns)
+      else (ls, nl, ns)) (acc, [], 0) list
   res;;
 
-accSublistsThatSumTo [] [2; 2; -2; 2; 0; 1] 4;;
+sublists [] [2; 2; -2; 2; 0; 1] 4;;
 
-let findPathsThatSumTo tree sum =
-  let rec aux paths current_path = function
+(* find paths in the 'tree' that sum to 'sum' *)
+let paths tree sum =
+  let rec aux paths path = function
     | Empty -> paths
-    | Node (x, left, right) -> 
-      let new_path = x :: current_path in
-      let new_paths = accSublistsThatSumTo paths new_path sum in
-      let new_left_paths = aux new_paths new_path left in
-      aux new_left_paths new_path right
+    | Node (x, left, right) ->
+      let new_path = x :: path in
+      let new_paths = sublists paths new_path sum in
+      let left_paths = aux new_paths new_path left in
+      aux left_paths new_path right
   in
   aux [] [] tree
 ;;
 
-findPathsThatSumTo test 0;;
+paths test 0;;
--- ocaml/iv/04_09.ml
+++ ocaml/iv/04_09.ml
@@ -2,6 +2,7 @@ open Core.Std
 
 type 'a tree = Empty | Node of 'a * 'a tree * 'a tree
 
+(* generate random tree of given depth *)
 let rec generate = function
   | 0 -> Empty
   | d -> Node (Random.int 5 - 2, (generate (d - 1)), (generate (d - 1)));;
@@ -10,27 +11,29 @@ Random.init 12345;;
 
 let test = generate 5;;
 
-let acc_sublists_that_sum_to acc list sum =
+(* add to 'acc' those sublists of 'list' that begin with head and sum to 'sum' *)
+let sublists acc list sum =
   let (res, _, _) = List.fold list ~init:(acc, [], 0) ~f:(fun (ls, l, s) x ->
       let ns = s + x in
       let nl = x :: l in
-      if ns = sum 
+      if ns = sum
       then (nl :: ls, nl, ns)
       else (ls, nl, ns)) in
   res;;
 
-acc_sublists_that_sum_to [] [2; 2; -2; 2; 0; 1] 4;;
+sublists [] [2; 2; -2; 2; 0; 1] 4;;
 
-let find_paths_that_sum_to tree sum =
-  let rec aux paths current_path = function
+(* find paths in the 'tree' that sum to 'sum' *)
+let paths tree sum =
+  let rec aux paths path = function
     | Empty -> paths
-    | Node (x, left, right) -> 
-      let new_path = x :: current_path in
-      let new_paths = acc_sublists_that_sum_to paths new_path sum in
-      let new_left_paths = aux new_paths new_path left in
-      aux new_left_paths new_path right
+    | Node (x, left, right) ->
+      let new_path = x :: path in
+      let new_paths = sublists paths new_path sum in
+      let left_paths = aux new_paths new_path left in
+      aux left_paths new_path right
   in
   aux [] [] tree
 ;;
 
-find_paths_that_sum_to test 0;;
+paths test 0;;
